f20557a157abede008e902427e3fe9b5f949ccc6
Anast <nastya_strong@ukr.net>
2019-02-21 10:49:59 +0200

    Add ObscurencyFinder field

---
+++ src/test/ObscurencyFinderTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import main.ObscurencyFinder;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class ObscurencyFinderTest {
+
+    private ObscurencyFinder unit;
+    @Before
+    public void setUp() throws Exception {
+        unit = new ObscurencyFinder();
+    }
+
+}
